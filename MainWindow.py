3350b49de3c9a6a13fa36a237fbb42c119af3e1b
sgarsztka <sbl86al.o@gmail.com>
2022-07-14 21:47:38 +0200

    Added panel with gpx track data

--- MainWindow.py
+++ MainWindow.py
@@ -10,8 +10,13 @@ from datetime import datetime
 import sys
 import MapMaker as map
 
+
 class MyBrowser(wx.Frame):
     loggedUser = ""
+    # dateOfRide = ""
+    avgHr = ""
+    avgSpeed = ""
+    distance = ""
     def __init__(self, *args, **kwargs):
         super(MyBrowser, self).__init__(*args, **kwargs)
         dlg = LoginDialog.LoginDialog()
@@ -54,12 +59,68 @@ class MyBrowser(wx.Frame):
         # tracksList = self.getUserTracks()
 
         self.lst = wx.ListBox(self, size=(150, -1), choices=dateList, style=wx.LB_SINGLE)
-        self.sb = wx.StaticBox(self, label='placeholder')
+        self.box = wx.StaticBox( self, wx.ID_ANY, "GPX Track Data", size=(200, -1))
+
+        self.trackData = wx.StaticBoxSizer(self.box, wx.VERTICAL )
+        self.dateSizer = wx.BoxSizer( wx.HORIZONTAL)
+        self.dateLabel = wx.StaticText(self, -1, "Date:")
+        self.dateSpacer = wx.StaticText(self, -1, "    ")
+        self.dateLabel2 = wx.StaticText(self, -1, "")
+        self.dateSizer.Add(self.dateLabel)
+        self.dateSizer.Add(self.dateSpacer)
+        self.dateSizer.Add(self.dateLabel2)
+
+        self.avgSpeedSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.avgSpeedLabel1 = wx.StaticText(self, -1, "AVG Speed:")
+        self.avgSpeedSpacer = wx.StaticText(self, -1, "    ")
+        self.avgSpeedLabel2 = wx.StaticText(self, -1, "")
+        self.avgSpeedSizer.Add(self.avgSpeedLabel1)
+        self.avgSpeedSizer.Add(self.avgSpeedSpacer)
+        self.avgSpeedSizer.Add(self.avgSpeedLabel2)
+
+        self.distanceSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.distanceLabel1 = wx.StaticText(self, -1, "Distance:")
+        self.distanceSpacer = wx.StaticText(self, -1, "    ")
+        self.distanceLabel2 = wx.StaticText(self, -1, "")
+        self.distanceSizer.Add(self.distanceLabel1)
+        self.distanceSizer.Add(self.distanceSpacer)
+        self.distanceSizer.Add(self.distanceLabel2)
+
+        self.hrSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.hrLabel1 = wx.StaticText(self, -1, "AVG Heartrate:")
+        self.hrSpacer = wx.StaticText(self, -1, "    ")
+        self.hrLabel2 = wx.StaticText(self, -1, "")
+        self.hrSizer.Add(self.hrLabel1)
+        self.hrSizer.Add(self.hrSpacer)
+        self.hrSizer.Add(self.hrLabel2)
+
+        self.rideTimeSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.rideTimeLabel1 = wx.StaticText(self, -1, "Ride time:")
+        self.rideTimeSpacer = wx.StaticText(self, -1, "    ")
+        self.rideTimeLabel2 = wx.StaticText(self, -1, "")
+        self.rideTimeSizer.Add(self.rideTimeLabel1)
+        self.rideTimeSizer.Add(self.rideTimeSpacer)
+        self.rideTimeSizer.Add(self.rideTimeLabel2)
+
+        self.pointsNumberSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.pointsNumberLabel1 = wx.StaticText(self, -1, "Number of points:")
+        self.pointsNumberSpacer = wx.StaticText(self, -1, "    ")
+        self.pointsNumberLabel2 = wx.StaticText(self, -1, "")
+        self.pointsNumberSizer.Add(self.pointsNumberLabel1)
+        self.pointsNumberSizer.Add(self.pointsNumberSpacer)
+        self.pointsNumberSizer.Add(self.pointsNumberLabel2)
 
         hbox.Add(self.lst, 0, wx.ALIGN_LEFT | wx.EXPAND, 10)
         hbox.Add(self.browser, 1, wx.EXPAND, 10)
-        vbox.Add(self.sb, 1,  wx.BOTTOM | wx.ALIGN_CENTER_HORIZONTAL, 10)
+        hbox.Add(self.trackData, 0, wx.EXPAND, 10)
+        self.trackData.Add(self.dateSizer, 0 ,wx.EXPAND, 10)
+        self.trackData.Add(self.avgSpeedSizer, 0 ,wx.EXPAND, 10)
+        self.trackData.Add(self.distanceSizer, 0, wx.EXPAND, 10)
+        self.trackData.Add(self.hrSizer, 0, wx.EXPAND, 10)
+        self.trackData.Add(self.rideTimeSizer, 0, wx.EXPAND, 10)
+        self.trackData.Add(self.pointsNumberSizer, 0, wx.EXPAND, 10)
         self.lst.Bind(wx.EVT_LISTBOX, self.getUserPoints, self.lst)
+
 
         self.SetSizer(vbox)
         self.SetSizer(hbox)
@@ -121,6 +182,7 @@ class MyBrowser(wx.Frame):
         a_object = datetime.strptime(positionSelected, '%Y-%m-%d %H:%M:%S')
 
         gpxPointsListTemp = self.getSelectedTrack(a_object)
+        # print(gpxPointsListTemp)
         gpxPointsList = []
         for index in range(len(gpxPointsListTemp)):
             for key in gpxPointsListTemp[index]:
@@ -136,13 +198,19 @@ class MyBrowser(wx.Frame):
                 lonPointsList.append(splittedList[i])
             else:
                 latPointsList.append(splittedList[i])
-        print(latPointsList)
-        print(lonPointsList)
+        # print(latPointsList)
+        # print(lonPointsList)
         formPointsList = [(latPointsList[i], lonPointsList[i]) for i in range(0, len(latPointsList))]
-        print(formPointsList[3])
+        print(gpxPointsList[2])
         map.updateMap(formPointsList)
         absPath = os.getcwd()
         self.browser.LoadURL(pathlib2.Path(absPath + "/" + "maparea.html").as_uri())
+        self.dateLabel2.SetLabel(positionSelected)
+        self.avgSpeedLabel2.SetLabel(str(gpxPointsList[2])[:5])
+        self.distanceLabel2.SetLabel(str(gpxPointsList[3])[:6])
+        self.hrLabel2.SetLabel(str(gpxPointsList[4])[:5])
+        self.rideTimeLabel2.SetLabel(str(gpxPointsList[6]))
+        self.pointsNumberLabel2.SetLabel(str(len(gpxPointsList[7])))
 
 def main():
     app = wx.App(False)
--- RegisterFrame.py
+++ RegisterFrame.py
@@ -2,46 +2,6 @@ import wx
 import sqlAlch
 
 
-# class RegisterFrame(wx.Dialog):
-#     def __init__(self):
-#         wx.Dialog.__init__(self, None, title="Register new user")
-#         user_sizer = wx.BoxSizer(wx.HORIZONTAL)
-#         user = wx.StaticText(self, label="User")
-#         user_sizer.Add(user, 0, wx.ALL | wx.CENTER, 5)
-#         self.user = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
-#         self.user.Bind(wx.EVT_TEXT_ENTER, self.onRegister)
-#         user_sizer.Add(self.user, 0, wx.ALL)
-#
-#         pass_sizer = wx.BoxSizer(wx.HORIZONTAL)
-#
-#         pass_lbl = wx.StaticText(self, label="Password:")
-#         pass_sizer.Add(pass_lbl, 0, wx.ALL | wx.CENTER, 5)
-#         self.password = wx.TextCtrl(self, style=wx.TE_PASSWORD | wx.TE_PROCESS_ENTER)
-#         self.password.Bind(wx.EVT_TEXT_ENTER, self.onRegister)
-#         pass_sizer.Add(self.password, 0, wx.ALL, 5)
-#
-#         main_sizer = wx.BoxSizer(wx.VERTICAL)
-#         main_sizer.Add(user_sizer, 0, wx.ALL, 5)
-#         main_sizer.Add(pass_sizer, 0, wx.ALL, 5)
-#
-#         btnRegister = wx.Button(self, label="Create User")
-#         btnRegister.Bind(wx.EVT_BUTTON, self.onRegister)
-#         main_sizer.Add(btnRegister, 0, wx.ALL | wx.CENTER, 5)
-#
-#         self.SetSizer(main_sizer)
-#
-#     def onRegister(self, event):
-#         user_password = self.password.GetValue()
-#         user_name = self.user.GetValue()
-#         sqlAlch.insertUser(user_name, user_password)
-#         if sqlAlch.checkUser(user_name, user_password):
-#             wx.MessageBox("User created!", "Message", wx.OK | wx.ICON_INFORMATION)
-#             self.Close()
-#
-#         else:
-#             wx.MessageBox("User not created properly", "Message", wx.OK | wx.ICON_ERROR)
-
-
 class RegisterFrame(wx.Dialog):
 
     def __init__(self):
